2a5c1f0530d1dc232c9d0ab181d1177c87f71fbc
André Pereira <andrepereira97@yahoo.com>
2024-04-13 00:17:12 -0300

    :tada: init project

---
+++ src/main/java/org/example/devweekchallenge/DevWeekChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.example.devweekchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWeekChallengeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DevWeekChallengeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/devweekchallenge/DevWeekChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.devweekchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWeekChallengeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
