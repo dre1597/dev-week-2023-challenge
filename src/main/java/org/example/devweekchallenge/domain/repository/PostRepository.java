4880ccbd07f386e2457cccd5b21e03a656737813
André Pereira <andrepereira97@yahoo.com>
2024-04-13 13:34:54 -0300

    :bug: fix repository inheritance

--- src/main/java/org/example/devweekchallenge/domain/repository/PostRepository.java
+++ src/main/java/org/example/devweekchallenge/domain/repository/PostRepository.java
@@ -1,9 +1,9 @@
 package org.example.devweekchallenge.domain.repository;
 
 import org.example.devweekchallenge.domain.model.Post;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends CrudRepository<Post, String> {
+public interface PostRepository extends JpaRepository<Post, String> {
 }
--- src/main/java/org/example/devweekchallenge/domain/repository/UserRepository.java
+++ src/main/java/org/example/devweekchallenge/domain/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package org.example.devweekchallenge.domain.repository;
 
 import org.example.devweekchallenge.domain.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
+  User findByEmail(String email);
 }
